58ce3ec86fa33a49e234583c386c1d697189a92d
Brian Chen <brian.chen@tabcorp.com.au>
2021-03-25 07:13:03 +1100

    will now wait for auth loaded before rendering main UI

--- frame/App/index.js
+++ frame/App/index.js
@@ -1,16 +1,41 @@
 // @flow
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 
+import { app } from '@core/store';
 import { Box } from '@pkgs/components';
+import { useFirebase } from '@pkgs/utils';
 
 import Body from './Body';
 import Header from './Header';
 
-const App = (): React.Node => (
-  <Box>
-    <Header />
-    <Body />
-  </Box>
-);
+const App = (): React.Node => {
+  const firebase = useFirebase();
+  const dispatch = useDispatch();
+
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      dispatch(app.actions.setAuthenticated(!!user));
+      !loaded && setLoaded(true);
+    });
+  }, []);
+
+  if (!loaded) {
+    return (
+      <div>
+        loading
+      </div>
+    );
+  }
+
+  return (
+    <Box>
+      <Header />
+      <Body />
+    </Box>
+  );
+};
 
 export default App;
