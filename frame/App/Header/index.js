bef97e28355dbbe6431f634d53de795339b36bbd
Jeremy <jeremyVDLRC@gmail.com>
2021-03-25 20:26:48 +1100

    centre logo text and added colour

--- frame/App/Header/index.js
+++ frame/App/Header/index.js
@@ -13,14 +13,33 @@ const Header = (): React.Node => {
       height: '50px',
       backgroundColor: 'red',
     },
+    headerText: {
+      display: 'inline-flex',
+      justifyContent: 'flex-end ',
+      width: '50%',
+    },
   };
 
   return (
     <Box style={styles.container}>
-      <Box>
-        Two Dew
+      <Box style={styles.headerText}>
+        <h1 style={{
+          fontSize: '40px', fontWeight: 600, margin: 0, padding: 0,
+        }}
+        >
+          Two
+        </h1>
+        <h1 style={{
+          color: 'white', fontSize: '40px', fontWeight: 600, margin: 0, padding: 0,
+        }}
+        >
+          Dew
+        </h1>
       </Box>
-      <AccountStatus />
+      <Box style={styles.headerText}>
+        <AccountStatus />
+      </Box>
+
     </Box>
   );
 };
