b289bc9df01ed2aecf9cdca5b69d538b58ed38a9
Brian Chen <brian.chen@tabcorp.com.au>
2021-04-20 13:20:08 +1000

    add button

--- packages/pkgs/components/index.js
+++ packages/pkgs/components/index.js
@@ -1,4 +1,5 @@
 // @flow
 export { default as Box } from './Box';
+export { default as Button } from './Button';
 export { default as ClickAwayListener } from './ClickAwayListener';
 export { default as Icon } from './Icon';
