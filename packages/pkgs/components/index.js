bf3c3f612eae26f172d58a1c989eb1634a2518aa
Jeremy <jeremyVDLRC@gmail.com>
2021-04-24 18:41:18 +1000

    add modal to components index

--- packages/pkgs/components/index.js
+++ packages/pkgs/components/index.js
@@ -1,5 +1,6 @@
 // @flow
 export { default as Box } from './Box';
+export { default as Modal } from './Modal';
 export { default as Button } from './Button';
 export { default as ClickAwayListener } from './ClickAwayListener';
 export { default as Icon } from './Icon';
